da41993fad5081e8d24eaa98a8a1cb59218f70f0
Marc Ramis <marc.ramis@enti.cat>
2021-11-28 17:06:25 +0100

    gitflow-feature-stash: Strategy

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 #include "SDL_SimpleApp.h"
 #include "ScenePathFindingMouse.h"
+#include "SceneStrategy.h"
 
 using namespace std;
 
@@ -40,6 +41,9 @@ int main(int argc, char ** argv)
 			}
 			if (event.key.keysym.scancode == SDL_SCANCODE_2)
 			{
+				delete(curr_scene);
+				curr_scene = new SceneStrategy;
+				app->setWindowTitle(curr_scene->getTitle());
 			}
 			if (event.key.keysym.scancode == SDL_SCANCODE_3)
 			{
