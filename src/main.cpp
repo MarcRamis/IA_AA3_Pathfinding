220aab5263e9a6c8b318323a3ec99d71294ef231
Marc Ramis <marc.ramis@enti.cat>
2021-11-29 19:11:31 +0100

    Finished: Scene start on ScenePathfindingMouse

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char ** argv)
 	
 	SDL_SimpleApp *app = SDL_SimpleApp::Instance();
 
-	Scene *curr_scene = new SceneStrategy;
+	Scene *curr_scene = new ScenePathFindingMouse;
 	app->setWindowTitle(curr_scene->getTitle());
 
 	
